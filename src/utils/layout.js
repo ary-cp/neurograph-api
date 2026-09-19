a33eda7c6c478977fca18675245f40a16b1f9138
ary-cp <b24bs1122@iitj.ac.in>
2026-09-20 03:42:50 +0530

    feat: layout clustering by node kind using dagre compound graphs

--- src/utils/layout.js
+++ src/utils/layout.js
@@ -11,11 +11,25 @@ const NODE_H = 120
 export function layoutGraph(nodes, edges, { origin = { x: 0, y: 0 }, direction = 'LR' } = {}) {
   if (nodes.length === 0) return nodes
 
-  const g = new dagre.graphlib.Graph()
+  const g = new dagre.graphlib.Graph({ compound: true })
   g.setDefaultEdgeLabel(() => ({}))
   g.setGraph({ rankdir: direction, nodesep: 56, ranksep: 140, marginx: 24, marginy: 24 })
 
-  for (const n of nodes) g.setNode(n.id, { width: NODE_W, height: NODE_H })
+  const clusters = new Set()
+  for (const n of nodes) {
+    g.setNode(n.id, { width: NODE_W, height: NODE_H })
+    
+    // Mentor feature: Cluster nodes by type/kind
+    const kind = n.data?.kind
+    if (kind) {
+      const clusterId = `cluster_${kind}`
+      if (!clusters.has(clusterId)) {
+        clusters.add(clusterId)
+        g.setNode(clusterId, { label: clusterId })
+      }
+      g.setParent(n.id, clusterId)
+    }
+  }
   for (const e of edges) if (g.hasNode(e.source) && g.hasNode(e.target)) g.setEdge(e.source, e.target)
 
   dagre.layout(g)
